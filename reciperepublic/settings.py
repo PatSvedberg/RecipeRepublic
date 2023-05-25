b226dee20313a8288c9b5da682d65c1e10f8f60a
PatSvedberg <pat.svedberg@gmail.com>
2023-05-25 07:11:43 +0000

    Heroku deployment test commit

--- reciperepublic/settings.py
+++ reciperepublic/settings.py
@@ -31,7 +31,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['recipe-republic.herokuapp.com', 'localhost','8000-patsvedberg-reciperepub-1t3vnxbhwyq.ws-eu97.gitpod.io']
 
